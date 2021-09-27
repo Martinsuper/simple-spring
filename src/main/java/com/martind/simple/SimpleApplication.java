fa4fd266964f3518349ae3a32d8e48f64aaee26b
duanluyao <duanluyao@imdada.cn>
2021-09-27 11:50:57 +0800

    最简单的springboot项目

---
+++ src/main/java/com/martind/simple/SimpleApplication.java
@@ -0,0 +1,17 @@
+package com.martind.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 一个简单的 SpringBoot 应用
+ * @Author: duanluyao
+ * @CreateTime: 2021/9/27 11:42:42
+ */
+@SpringBootApplication
+public class SimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
+    }
+}
